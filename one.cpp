6b6b6861097e847bebf24ed346c75400a5d01d30
Tushar Saxena <stushar345@gmail.com>
2021-03-24 15:38:06 +0530

    Update one.cpp

--- one.cpp
+++ one.cpp
@@ -1,6 +1,8 @@
-int romanToDecimal(string &s) 
-{
+#include<bits/stdc++.h>
+using namespace std;
 
+int roman_to_decimal(string s)
+{
     unordered_map<char, int> T = { { 'I' , 1 },
                                    { 'V' , 5 },
                                    { 'X' , 10 },
@@ -8,19 +10,25 @@ int romanToDecimal(string &s)
                                    { 'C' , 100 },
                                    { 'D' , 500 },
                                    { 'M' , 1000 } };
-                                   
-   int sum = T[s[s.size()-1]];
-   for (int i = s.length() - 2; i >= 0; --i) 
-   {
-       if (T[s[i]] < T[s[i + 1]])
-       {
-           sum -= T[s[i]];
-       }
-       else
-       {
-           sum += T[s[i]];
-       }
-   }
-   
-   return sum;
+
+
+    int n=s.size();
+    int sum=T[s[n-1]];
+
+    for(int i=n-2;i>=0;i--)
+    {
+        if(T[s[i]]<T[s[i+1]])
+            sum-=T[s[i]];
+        else
+            sum+=T[s[i]];
+    }
+
+    return sum;
+    
+}
+
+int main()
+{
+    string s="VIII";
+    cout<<roman_to_decimal(s);
 }
